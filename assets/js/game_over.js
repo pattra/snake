90a1745416f803afe1d8f96d76ef5db0d3c4554d
pattra <pattra.a@gmail.com>
2016-05-01 18:37:56 -0400

    styling

--- assets/js/game_over.js
+++ assets/js/game_over.js
@@ -7,6 +7,7 @@ const Game_Over = {
     game.stage.backgroundColor = '#fff';
     let style = { font: 'normal 16px "Press Start 2P"'};
     let xOffset = score > 99 ? 125 : 120;
+    xOffset = score === 0 ? 110 : xOffset;
 
     let titleText = game.add.text(game.world.centerX - xOffset, game.world.centerY - 40, `FINAL SCORE: ${score}`, style);
     let startText = game.add.text(game.world.centerX - 90, game.world.centerY, 'try again ?', style);
